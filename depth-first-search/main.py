08bb072b37e16dea487e16d0fe1aa50c0674c0c0
Vitoria Ellen <viviellenh@alu.ufc.br>
2023-10-28 21:16:15 -0300

    adicionando tratamento de erro ao não encontrar arquivo desejado

--- depth-first-search/main.py
+++ depth-first-search/main.py
@@ -1,18 +1,19 @@
-# main.py
 from imprime import imprime
 from loadlista import loadlista
 from dfs import DFS, DFS_visit
 
 [lista_adj, N] = loadlista()
 
-graus_saida = [len(lista_adj[u]) for u in range(N)]
-vertice_maior_saida = graus_saida.index(max(graus_saida))
-V = [vertice_maior_saida] + [u for u in range(N) if u != vertice_maior_saida]
+if N is not None:
+    graus_saida = [len(lista_adj[u]) for u in range(N)]
+    vertice_maior_saida = graus_saida.index(max(graus_saida))
+    V = [vertice_maior_saida] + [u for u in range(N) if u != vertice_maior_saida]
 
-cor = ["Branco"] * N
-d = [0] * N
-f = [0] * N
-mark = 0
-mark = DFS(V, cor, lista_adj, d, f, mark)
-imprime(d, f, N)
-
+    cor = ["Branco"] * N
+    d = [0] * N
+    f = [0] * N
+    mark = 0
+    mark = DFS(V, cor, lista_adj, d, f, mark)
+    imprime(d, f, N)
+else:
+    print("Nao foi possivel carregar o arquivo, encerrando o programa.")
